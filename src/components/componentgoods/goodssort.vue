4ba7f4f01eaabdc301cc421ab8c1aed7388cf897
sswkfront <1135769417@qq.com>
2018-08-09 17:59:58 +0800

    提交修改

--- src/components/componentgoods/goodssort.vue
+++ src/components/componentgoods/goodssort.vue
@@ -3,23 +3,84 @@
 		<div class="swiper-wrapper">
 	        <div class="swiper-slide">
 	        	<el-row>
-	        		<el-col :span="4" class="sort_list">
-	        			<div></div>
-	        			<span>111</span>
+	        		<el-col :span="5" class="sort_list">
+	        			<div>
+	        				<img src="//fuss10.elemecdn.com/7/d8/a867c870b22bc74c87c348b75528djpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" class="img_wh">
+	        			</div>
+	        			<span>美食</span>
 	        		</el-col>
-	        		<el-col :span="4" class="sort_list"></el-col>
-	        		<el-col :span="4" class="sort_list"></el-col>
-	        		<el-col :span="4" class="sort_list"></el-col>
-	        		<el-col :span="4" class="sort_list"></el-col>
-	        		<el-col :span="4" class="sort_list"></el-col>
+	        		<el-col :span="5" class="sort_list">
+	        			<div>
+	        				<img src="//fuss10.elemecdn.com/c/7e/76a23eb90dada42528bc41499d6f8jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" class="img_wh"/>
+	        				
+	        			</div>
+	        			<span>商超便利</span>
+	        			
+	        		</el-col>
+	        		<el-col :span="4" class="sort_list">
+	        			<div>
+	        				<img src="//fuss10.elemecdn.com/c/e9/8b589472823fa97666cef19af644cjpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" class="img_wh"/>
+	        				
+	        			</div>
+	        			<span>午餐</span>
+	        		</el-col>
+	        		<el-col :span="5" class="sort_list">
+	        			<div>
+	        				<img src="//fuss10.elemecdn.com/0/d0/dd7c960f08cdc756b1d3ad54978fdjpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" class="img_wh"/>
+	        				
+	        			</div>
+	        			<span>水果</span>
+	        		</el-col>
+	        		<el-col :span="5" class="sort_list">
+	        			<div>
+	        				<img src="//fuss10.elemecdn.com/7/0a/af108e256ebc9f02db599592ae655jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" class="img_wh"/>
+	        				
+	        			</div>
+	        			<span>医药健康</span>
+	        		</el-col>
+	        	</el-row>
+	        	<el-row>
+	        		<el-col :span="5" class="sort_list">
+	        			<div>
+	        				<img src="//fuss10.elemecdn.com/7/d8/a867c870b22bc74c87c348b75528djpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" class="img_wh">
+	        			</div>
+	        			<span>美食</span>
+	        		</el-col>
+	        		<el-col :span="5" class="sort_list">
+	        			<div>
+	        				<img src="//fuss10.elemecdn.com/c/7e/76a23eb90dada42528bc41499d6f8jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" class="img_wh"/>
+	        				
+	        			</div>
+	        			<span>商超便利</span>
+	        			
+	        		</el-col>
+	        		<el-col :span="4" class="sort_list">
+	        			<div>
+	        				<img src="//fuss10.elemecdn.com/c/e9/8b589472823fa97666cef19af644cjpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" class="img_wh"/>
+	        				
+	        			</div>
+	        			<span>午餐</span>
+	        		</el-col>
+	        		<el-col :span="5" class="sort_list">
+	        			<div>
+	        				<img src="//fuss10.elemecdn.com/0/d0/dd7c960f08cdc756b1d3ad54978fdjpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" class="img_wh"/>
+	        				
+	        			</div>
+	        			<span>水果</span>
+	        		</el-col>
+	        		<el-col :span="5" class="sort_list">
+	        			<div>
+	        				<img src="//fuss10.elemecdn.com/7/0a/af108e256ebc9f02db599592ae655jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" class="img_wh"/>
+	        				
+	        			</div>
+	        			<span>医药健康</span>
+	        		</el-col>
 	        	</el-row>
 	        </div>
 	        <div class="swiper-slide">Slide 2</div>
-	        <div class="swiper-slide">Slide 3</div>
 	    </div>
 		<div class="swiper-pagination"></div>
 	</div>
-	
 </template>
 
 <script>
@@ -47,7 +108,9 @@
 <style scoped>
 	.goodssort_box{
 		width: 100%;
-		height: 160px;
+		height: 180px;
+		box-sizing: border-box;
+		overflow: hidden;
 	}
 	.swiper-wrapper{
 		width: 100%;
@@ -56,15 +119,26 @@
 	.swiper-wrapper>div{
 		width: 100%;
 		height: 100%;
+		
 	}
 	.sort_list{
 		height: 80px;
 		background: #ffffff;
+		
 	}
 	.sort_list>div{
 		width: 60px;
 		height: 60px;
-		background: red;
 		margin: 0 auto;
+	}
+	.sort_list>span{
+		font-size: 12px;
+	}
+	.img_wh{
+		width: 100%;
+		height: 100%;
+	}
+	.swiper-pagination{
+		bottom: 0;
 	}
 </style>
--- src/components/componentgoods/indexgoods.vue
+++ src/components/componentgoods/indexgoods.vue
@@ -1,6 +1,30 @@
 <template>
-	<div class="goos_box">
+	<div class="goods_box">
 		<goodssort></goodssort>
+		<el-row style="padding: 10px 0;">
+			<i class="el-icon-minus"></i> 推荐商家 <i class="el-icon-minus"></i>
+		</el-row>
+		<div class="goods_dropdown">
+			<el-menu>
+				<el-menu-item index="1" class="screen-list">
+					<el-dropdown trigger="click" @command="handleCommand">
+						<div class="el-dropdown-link">
+				        	{{dropdown}}<i class="el-icon-arrow-down el-icon--right"></i>
+				     	</div>
+				     	<el-dropdown-menu slot="dropdown">
+					        <el-dropdown-item command="综合排序">综合排序</el-dropdown-item>
+					        <el-dropdown-item command="好评优先">好评优先</el-dropdown-item>
+					        <el-dropdown-item command="销量最高">销量最高</el-dropdown-item>
+					        <el-dropdown-item command="配送最快">配送最快</el-dropdown-item>
+					        <el-dropdown-item command="配送费最低">配送费最低</el-dropdown-item>
+					    </el-dropdown-menu>
+					</el-dropdown>
+				</el-menu-item>
+				<el-menu-item index="2" class="screen-list">距离最近</el-menu-item>
+				<el-menu-item index="3" class="screen-list">品质联盟</el-menu-item>
+				<el-menu-item index="4" class="screen-list">筛选</el-menu-item>
+			</el-menu>
+		</div>
 	</div>
 	
 </template>
@@ -11,19 +35,46 @@
 		name:"goods",
 		data(){
 			return {
-				name:"goods"
+				name:"goods",
+				dropdown:'综合排序'
 			}
 		},
 		components:{
 			goodssort:goodssort
+		},
+		methods:{
+			handleCommand(command) {
+		    	this.dropdown=command;
+		    },
+		    handleCheck(index){
+		    	console.log(index)
+		    }
 		}
 	}
 	
 </script>
 
 <style scoped>
-	.goos_box{
+	.goods_box{
 		width: 100%;
-		background: #0085FF;
+		background: #ffffff;
+		/*padding: 0 20px;*/
+		box-sizing: border-box;
+		overflow: hidden;
 	}
+	.goods_dropdown{
+		width: 100%;
+		color: #666;
+		font-weight:600;
+		
+			
+	}
+	.screen-list{
+		height: 100%;
+		width: 25%;
+		float: left;
+		line-height: 30px;
+		color: #666;
+	}
+	
 </style>
